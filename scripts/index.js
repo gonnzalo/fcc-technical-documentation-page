b92e24d33d467df5a48dd976ee2dcbd3ee329a77
Gonzalo <arecogonzalo@gmail.com>
2019-01-30 17:56:33 +0800

    finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,24 @@
-const currentLinks = document.querySelectorAll('a li')
+const currentLinks = document.querySelectorAll("a li");
+const mediaquery = window.matchMedia("(max-width: 1250px)");
+const navBarToggle = document.getElementById("js-navbar-toggle");
+const navBar = document.getElementById("navbar");
+const doc = document.getElementById("main-doc");
 
-// window.addEventListener('click', currentLinks.forEach( (link) => {
-//     console.log(link);
-//     // link.classList.add("active")
-// }))
+currentLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    currentLinks.forEach(i => i.classList.remove("active"));
+    link.classList.add("active");
 
+    if (mediaquery.matches) {
+      navBar.classList.toggle("navbar-active");
+      doc.classList.toggle("main-active");
+      navBarToggle.classList.toggle("open");
+    }
+  });
+});
 
-currentLinks.forEach((link) =>{
-    link.addEventListener('click', () => { 
-        currentLinks.forEach( i => i.classList.remove('active') )
-        link.classList.add("active");
-})
-})
+navBarToggle.addEventListener("click", () => {
+  navBar.classList.toggle("navbar-active");
+  doc.classList.toggle("main-active");
+  navBarToggle.classList.toggle("open");
+});
